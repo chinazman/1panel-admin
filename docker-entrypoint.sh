4bd8e4a7d708c92eed7cec99d8d6d4c95f5a69b7
leizuman <leizuman@fornewtech.com>
2024-12-19 19:47:20 +0800

    第一次才执行初始化

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-# 部署数据库
-npm run db:deploy
-
+if [ ! -f "/app/db/1panel.db" ]; then
+  # 部署数据库
+  npx prisma migrate deploy
+  npx tsx prisma/init-db.ts
+else
+  npx prisma migrate deploy
+fi
 # 启动应用
 exec node server.js 
